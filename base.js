9d72c43cebcc22994de1411e83054c1ce1b4a804
harrycapta <harrycapta@gmail.com>
2024-08-02 08:52:15 +0200

    three

--- base.js
+++ base.js
@@ -1,80 +1,383 @@
-;(function () {
-    // Dark mode toggle
-    // Look specifically for 'false' so we don't break existing sites that don't have this attribute
-    const showDarkModeSwitch =
-      document.body.getAttribute('data-dark-mode-toggle') !== 'false'
-  
-    if (showDarkModeSwitch) {
-      const themeButtonContents = `
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-        class="theme-toggle__icon theme-toggle__icon--light"
-        style="width: 1.5rem; height: 1.5rem;"
-      >
-        <path
-          stroke-linecap="round" stroke-linejoin="round"
-          d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25
-            12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"/>
-      </svg>
-  
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
-        class="theme-toggle__icon theme-toggle__icon--dark"
-        style="width: 1.5rem; height: 1.5rem;"
-      >
-        <path stroke-linecap="round" stroke-linejoin="round"
-          d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003
-            11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"/>
-      </svg>
-    `
-      const themeButton = document.createElement('button')
-      themeButton.classList.add('theme-toggle')
-      themeButton.setAttribute('aria-label', 'Toggle theme')
-      themeButton.innerHTML = themeButtonContents
-      document.body.prepend(themeButton)
-  
-      const osTheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'light'
-      document.body.dataset.theme = localStorage.getItem('theme') ?? osTheme
-  
-      themeButton.addEventListener('click', () => {
-        if (
-          document.body.dataset.theme === 'dark' ||
-          (!localStorage.getItem('theme') && osTheme === 'dark')
-        ) {
-          document.body.dataset.theme = 'light'
-          localStorage.setItem('theme', 'light')
-        } else {
-          document.body.dataset.theme = 'dark'
-          localStorage.setItem('theme', 'dark')
-        }
-      })
-    }
-  
-    // Mobile menu
-    const hamburgerButton = document.querySelector('.navbar__hamburger button')
-    const hamburgerIcon = document.querySelector(
-      '.navbar__mobile-dropdown-open-icon'
-    )
-    const closeIcon = document.querySelector(
-      '.navbar__mobile-dropdown-close-icon'
-    )
-    const mobileDropdown = document.querySelector('.navbar__mobile-dropdown')
-  
-    if (hamburgerButton && hamburgerIcon && closeIcon && mobileDropdown) {
-      hamburgerButton.onclick = () => {
-        const isOpen =
-          hamburgerButton.getAttribute('aria-expanded') === 'true' || false
-        hamburgerButton.setAttribute('aria-expanded', !isOpen)
-        mobileDropdown.style.gridTemplateRows = isOpen ? '0fr' : '1fr'
-        mobileDropdown.style.visibility = isOpen ? 'hidden' : 'visible'
-        hamburgerIcon.style.display = isOpen ? 'block' : 'none'
-        closeIcon.style.display = isOpen ? 'none' : 'block'
-      }
-    }
-  
-    // Ensure CK Top Bar height is set correctly for browsers that don't support CSS :has() selector
-    const ckBar = document.querySelector('#coinkite-banner')
-    if (ckBar) {
-      document.documentElement.style.setProperty('--ck-top-bar-height', '35px')
-    }
-  })()
+const preload = () => {
+
+  let manager = new THREE.LoadingManager();
+  manager.onLoad = function() { 
+    const environment = new Environment( typo, particle );
+  }
+
+  var typo = null;
+  const loader = new THREE.FontLoader( manager );
+  const font = loader.load('https://res.cloudinary.com/dydre7amr/raw/upload/v1612950355/font_zsd4dr.json', function ( font ) { typo = font; });
+  const particle = new THREE.TextureLoader( manager ).load( 'https://res.cloudinary.com/dfvtkoboz/image/upload/v1605013866/particle_a64uzf.png');
+
+}
+
+if ( document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll))
+  preload ();
+else
+  document.addEventListener("DOMContentLoaded", preload ); 
+
+class Environment {
+
+  constructor( font, particle ){ 
+
+    this.font = font;
+    this.particle = particle;
+    this.container = document.querySelector( '#magic' );
+    this.scene = new THREE.Scene();
+    this.createCamera();
+    this.createRenderer();
+    this.setup()
+    this.bindEvents();
+  }
+
+  bindEvents(){
+
+    window.addEventListener( 'resize', this.onWindowResize.bind( this ));
+    
+  }
+
+  setup(){ 
+
+    this.createParticles = new CreateParticles( this.scene, this.font,             this.particle, this.camera, this.renderer );
+  }
+
+  render() {
+    
+     this.createParticles.render()
+     this.renderer.render( this.scene, this.camera )
+  }
+
+  createCamera() {
+
+    this.camera = new THREE.PerspectiveCamera( 65, this.container.clientWidth /  this.container.clientHeight, 1, 10000 );
+    this.camera.position.set( 0,0, 100 );
+
+  }
+
+  createRenderer() {
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( this.container.clientWidth, this.container.clientHeight );
+
+    this.renderer.setPixelRatio( Math.min( window.devicePixelRatio, 2));
+
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.container.appendChild( this.renderer.domElement );
+
+    this.renderer.setAnimationLoop(() => { this.render() })
+
+  }
+
+  onWindowResize(){
+
+    this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize( this.container.clientWidth, this.container.clientHeight );
+
+  }
+}
+
+class CreateParticles {
+	
+	constructor( scene, font, particleImg, camera, renderer ) {
+    
+		this.scene = scene;
+		this.font = font;
+		this.particleImg = particleImg;
+		this.camera = camera;
+		this.renderer = renderer;
+		
+		this.raycaster = new THREE.Raycaster();
+		this.mouse = new THREE.Vector2(-200, 200);
+		
+		this.colorChange = new THREE.Color();
+
+		this.buttom = false;
+
+		this.data = {
+
+			text: 'FUTURE\nIS NOW',
+			amount: 1500,
+			particleSize: 1,
+			particleColor: 0xffffff,
+			textSize: 16,
+			area: 250,
+			ease: .05,
+		}
+
+		this.setup();
+		this.bindEvents();
+
+	}
+
+
+	setup(){
+
+		const geometry = new THREE.PlaneGeometry( this.visibleWidthAtZDepth( 100, this.camera ), this.visibleHeightAtZDepth( 100, this.camera ));
+		const material = new THREE.MeshBasicMaterial( { color: 0x00ff00, transparent: true } );
+		this.planeArea = new THREE.Mesh( geometry, material );
+		this.planeArea.visible = false;
+		this.createText();
+
+	}
+
+	bindEvents() {
+
+		document.addEventListener( 'mousedown', this.onMouseDown.bind( this ));
+		document.addEventListener( 'mousemove', this.onMouseMove.bind( this ));
+		document.addEventListener( 'mouseup', this.onMouseUp.bind( this ));
+		
+	}
+
+	onMouseDown(){
+		
+		this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+		this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+		const vector = new THREE.Vector3( this.mouse.x, this.mouse.y, 0.5);
+		vector.unproject( this.camera );
+		const dir = vector.sub( this.camera.position ).normalize();
+		const distance = - this.camera.position.z / dir.z;
+		this.currenPosition = this.camera.position.clone().add( dir.multiplyScalar( distance ) );
+		
+		const pos = this.particles.geometry.attributes.position;
+		this.buttom = true;
+		this.data.ease = .01;
+		
+	}
+
+	onMouseUp(){
+
+		this.buttom = false;
+		this.data.ease = .05;
+	}
+
+	onMouseMove( ) { 
+
+	    this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	    this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+	}
+
+	render( level ){ 
+
+		const time = ((.001 * performance.now())%12)/12;
+		const zigzagTime = (1 + (Math.sin( time * 2 * Math.PI )))/6;
+
+		this.raycaster.setFromCamera( this.mouse, this.camera );
+
+		const intersects = this.raycaster.intersectObject( this.planeArea );
+
+		if ( intersects.length > 0 ) {
+
+			const pos = this.particles.geometry.attributes.position;
+			const copy = this.geometryCopy.attributes.position;
+			const coulors = this.particles.geometry.attributes.customColor;
+			const size = this.particles.geometry.attributes.size;
+
+		    const mx = intersects[ 0 ].point.x;
+		    const my = intersects[ 0 ].point.y;
+		    const mz = intersects[ 0 ].point.z;
+
+		    for ( var i = 0, l = pos.count; i < l; i++) {
+
+		    	const initX = copy.getX(i);
+		    	const initY = copy.getY(i);
+		    	const initZ = copy.getZ(i);
+
+		    	let px = pos.getX(i);
+		    	let py = pos.getY(i);
+		    	let pz = pos.getZ(i);
+
+		    	this.colorChange.setHSL( .5, 1 , 1 )
+		    	coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
+		    	coulors.needsUpdate = true;
+
+		    	size.array[ i ]  = this.data.particleSize;
+		    	size.needsUpdate = true;
+
+		    	let dx = mx - px;
+		    	let dy = my - py;
+		    	const dz = mz - pz;
+
+		    	const mouseDistance = this.distance( mx, my, px, py )
+		    	let d = ( dx = mx - px ) * dx + ( dy = my - py ) * dy;
+		    	const f = - this.data.area/d;
+
+		    	if( this.buttom ){ 
+
+		    		const t = Math.atan2( dy, dx );
+		    		px -= f * Math.cos( t );
+		    		py -= f * Math.sin( t );
+
+		    		this.colorChange.setHSL( .5 + zigzagTime, 1.0 , .5 )
+		    		coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
+		    		coulors.needsUpdate = true;
+
+		    		if ((px > (initX + 70)) || ( px < (initX - 70)) || (py > (initY + 70) || ( py < (initY - 70)))){
+
+		    			this.colorChange.setHSL( .15, 1.0 , .5 )
+		    			coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
+		    			coulors.needsUpdate = true;
+
+		    		}
+
+		    	}else{
+		    	
+			    	if( mouseDistance < this.data.area ){
+
+			    		if(i%5==0){
+
+			    			const t = Math.atan2( dy, dx );
+			    			px -= .03 * Math.cos( t );
+			    			py -= .03 * Math.sin( t );
+
+			    			this.colorChange.setHSL( .15 , 1.0 , .5 )
+			    			coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
+			    			coulors.needsUpdate = true;
+
+							size.array[ i ]  =  this.data.particleSize /1.2;
+							size.needsUpdate = true;
+
+			    		}else{
+
+					    	const t = Math.atan2( dy, dx );
+					    	px += f * Math.cos( t );
+					    	py += f * Math.sin( t );
+
+					    	pos.setXYZ( i, px, py, pz );
+					    	pos.needsUpdate = true;
+
+					    	size.array[ i ]  = this.data.particleSize * 1.3 ;
+					    	size.needsUpdate = true;
+				    	}
+
+			    		if ((px > (initX + 10)) || ( px < (initX - 10)) || (py > (initY + 10) || ( py < (initY - 10)))){
+
+			    			this.colorChange.setHSL( .15, 1.0 , .5 )
+			    			coulors.setXYZ( i, this.colorChange.r, this.colorChange.g, this.colorChange.b )
+			    			coulors.needsUpdate = true;
+
+			    			size.array[ i ]  = this.data.particleSize /1.8;
+			    			size.needsUpdate = true;
+
+			    		}
+			    	}
+
+		    	}
+
+		    	px += ( initX  - px ) * this.data.ease;
+		    	py += ( initY  - py ) * this.data.ease;
+		    	pz += ( initZ  - pz ) * this.data.ease;
+
+		    	pos.setXYZ( i, px, py, pz );
+		    	pos.needsUpdate = true;
+
+		    }
+		}
+	}
+
+	createText(){ 
+
+		let thePoints = [];
+
+		let shapes = this.font.generateShapes( this.data.text , this.data.textSize  );
+		let geometry = new THREE.ShapeGeometry( shapes );
+		geometry.computeBoundingBox();
+	
+		const xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+		const yMid =  (geometry.boundingBox.max.y - geometry.boundingBox.min.y)/2.85;
+
+		geometry.center();
+
+		let holeShapes = [];
+
+		for ( let q = 0; q < shapes.length; q ++ ) {
+
+			let shape = shapes[ q ];
+
+			if ( shape.holes && shape.holes.length > 0 ) {
+
+				for ( let  j = 0; j < shape.holes.length; j ++ ) {
+
+					let  hole = shape.holes[ j ];
+					holeShapes.push( hole );
+				}
+			}
+
+		}
+		shapes.push.apply( shapes, holeShapes );
+
+		let colors = [];
+		let sizes = [];
+					
+		for ( let  x = 0; x < shapes.length; x ++ ) {
+
+			let shape = shapes[ x ];
+
+			const amountPoints = ( shape.type == 'Path') ? this.data.amount/2 : this.data.amount;
+
+			let points = shape.getSpacedPoints( amountPoints ) ;
+
+			points.forEach( ( element, z ) => {
+						
+				const a = new THREE.Vector3( element.x, element.y, 0 );
+				thePoints.push( a );
+				colors.push( this.colorChange.r, this.colorChange.g, this.colorChange.b);
+				sizes.push( 1 )
+
+				});
+		}
+
+		let geoParticles = new THREE.BufferGeometry().setFromPoints( thePoints );
+		geoParticles.translate( xMid, yMid, 0 );
+				
+		geoParticles.setAttribute( 'customColor', new THREE.Float32BufferAttribute( colors, 3 ) );
+		geoParticles.setAttribute( 'size', new THREE.Float32BufferAttribute( sizes, 1) );
+
+		const material = new THREE.ShaderMaterial( {
+
+			uniforms: {
+				color: { value: new THREE.Color( 0xffffff ) },
+				pointTexture: { value: this.particleImg }
+			},
+			vertexShader: document.getElementById( 'vertexshader' ).textContent,
+			fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+
+			blending: THREE.AdditiveBlending,
+			depthTest: false,
+			transparent: true,
+		} );
+
+		this.particles = new THREE.Points( geoParticles, material );
+		this.scene.add( this.particles );
+
+		this.geometryCopy = new THREE.BufferGeometry();
+		this.geometryCopy.copy( this.particles.geometry );
+		
+	}
+
+	visibleHeightAtZDepth ( depth, camera ) {
+
+	  const cameraOffset = camera.position.z;
+	  if ( depth < cameraOffset ) depth -= cameraOffset;
+	  else depth += cameraOffset;
+
+	  const vFOV = camera.fov * Math.PI / 180; 
+
+	  return 2 * Math.tan( vFOV / 2 ) * Math.abs( depth );
+	}
+
+	visibleWidthAtZDepth( depth, camera ) {
+
+	  const height = this.visibleHeightAtZDepth( depth, camera );
+	  return height * camera.aspect;
+
+	}
+
+	distance (x1, y1, x2, y2){
+	   
+	    return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
+	}
+}
